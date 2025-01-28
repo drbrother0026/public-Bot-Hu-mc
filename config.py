3ca331f4f0e6d66a848ead45a7e2ff1cfd41f294
drbrother0026 <testtrial129@gmail.com>
2025-01-28 12:45:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("8013090789:AAGwVR_5bEGoZGcOhHG-7jc4bxmmubG--bU")
+BOT_TOKEN=("8126998822:AAGa_MS6hRHJe_MBRaNJRzt5O66WBqhxxXQ")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("Riyahacksyt")
+OWNER_USERNAME=("terminator094")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6479495033")
+ADMIN_IDS=("6255940123")
 
 
 
